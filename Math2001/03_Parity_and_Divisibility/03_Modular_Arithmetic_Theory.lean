846a0d487e28385dd186e97fad34720b164a983c
Chris Dawes <cmsd2@cantab.net>
2025-12-29 18:53:54 +0000

    mod stuff

--- Math2001/03_Parity_and_Divisibility/03_Modular_Arithmetic_Theory.lean
+++ Math2001/03_Parity_and_Divisibility/03_Modular_Arithmetic_Theory.lean
@@ -10,7 +10,8 @@ example : 11 ≡ 3 [ZMOD 4] := by
   numbers
 
 example : -5 ≡ 1 [ZMOD 3] := by
-  sorry
+  use -2
+  numbers
 
 theorem Int.ModEq.add {n a b c d : ℤ} (h1 : a ≡ b [ZMOD n]) (h2 : c ≡ d [ZMOD n]) :
     a + c ≡ b + d [ZMOD n] := by
@@ -23,13 +24,25 @@ theorem Int.ModEq.add {n a b c d : ℤ} (h1 : a ≡ b [ZMOD n]) (h2 : c ≡ d [Z
     _ = n * x + n * y := by rw [hx, hy]
     _ = n * (x + y) := by ring
 
-
 theorem Int.ModEq.sub {n a b c d : ℤ} (h1 : a ≡ b [ZMOD n]) (h2 : c ≡ d [ZMOD n]) :
     a - c ≡ b - d [ZMOD n] := by
-  sorry
+  dsimp [Int.ModEq] at *
+  obtain ⟨x, hx⟩ := h1
+  obtain ⟨y, hy⟩ := h2
+  use x - y
+  calc
+    a - c - (b - d) = (a - b) - (c - d) := by ring
+    _ = n * x - n * y := by rw [hx, hy]
+    _ = n * (x - y) := by ring
 
 theorem Int.ModEq.neg {n a b : ℤ} (h1 : a ≡ b [ZMOD n]) : -a ≡ -b [ZMOD n] := by
-  sorry
+  dsimp [Int.ModEq] at *
+  obtain ⟨x, hx⟩ := h1
+  use -x
+  calc
+    (-a) - (-b) = -(a - b) := by ring
+    _ = - (n * x) := by rw [hx]
+    _ = n * (-x) := by ring
 
 theorem Int.ModEq.mul {n a b c d : ℤ} (h1 : a ≡ b [ZMOD n]) (h2 : c ≡ d [ZMOD n]) :
     a * c ≡ b * d [ZMOD n] := by
@@ -41,7 +54,6 @@ theorem Int.ModEq.mul {n a b c d : ℤ} (h1 : a ≡ b [ZMOD n]) (h2 : c ≡ d [Z
     _ = n * x * c + b * (n * y) := by rw [hx, hy]
     _ = n * (x * c + b * y) := by ring
 
-
 theorem Int.ModEq.pow_two (h : a ≡ b [ZMOD n]) : a ^ 2 ≡ b ^ 2 [ZMOD n] := by
   obtain ⟨x, hx⟩ := h
   use x * (a + b)
@@ -50,9 +62,13 @@ theorem Int.ModEq.pow_two (h : a ≡ b [ZMOD n]) : a ^ 2 ≡ b ^ 2 [ZMOD n] := b
     _ = n * x * (a + b) := by rw [hx]
     _ = n * (x * (a + b)) := by ring
 
-
 theorem Int.ModEq.pow_three (h : a ≡ b [ZMOD n]) : a ^ 3 ≡ b ^ 3 [ZMOD n] := by
-  sorry
+  obtain ⟨x, hx⟩ := h
+  use x * (a ^ 2 + b * a + b ^ 2)
+  calc
+    a ^ 3 - b ^ 3 = (a - b) * (a ^ 2 + b * a + b ^ 2) := by ring
+    _ = n * x * (a ^ 2 + b * a + b ^ 2) := by rw[hx]
+    _ = n * (x * (a ^ 2 + b * a + b ^ 2)) := by ring
 
 theorem Int.ModEq.pow (k : ℕ) (h : a ≡ b [ZMOD n]) : a ^ k ≡ b ^ k [ZMOD n] :=
   sorry -- we'll prove this later in the book
@@ -94,25 +110,59 @@ example {a b : ℤ} (ha : a ≡ 2 [ZMOD 4]) :
 
 
 example : 34 ≡ 104 [ZMOD 5] := by
-  sorry
+  use -14
+  calc
+    34 - 104 = -70 := by numbers
+    _ = 5 * -14 := by numbers
 
 theorem Int.ModEq.symm (h : a ≡ b [ZMOD n]) : b ≡ a [ZMOD n] := by
-  sorry
+  obtain ⟨x, hx⟩ := h
+  use -x
+  calc
+    b - a = -1 * (a - b) := by ring
+    _ = -1 * (n * x) := by rw [hx]
+    _ = n * -x := by ring
 
 theorem Int.ModEq.trans (h1 : a ≡ b [ZMOD n]) (h2 : b ≡ c [ZMOD n]) :
     a ≡ c [ZMOD n] := by
-  sorry
+  obtain ⟨x, hx⟩ := h1
+  obtain ⟨y, hy⟩ := h2
+  use x + y
+  calc
+    a - c = (a - b) + (b - c) := by ring
+    _ = n * x + n * y := by rw[hx, hy]
+    _ = n * (x + y) := by ring
 
 example : a + n * c ≡ a [ZMOD n] := by
-  sorry
+  use c
+  calc
+    a + n * c - a = n * c := by ring
 
+example {a b c d: ℤ} (h1 : a ≡ b [ZMOD n]) (h2 : c ≡ d [ZMOD n]) : ¬(a ⧸ c ≡ b ⧸ d [ZMOD n]) := by
+  sorry
 
 example {a b : ℤ} (h : a ≡ b [ZMOD 5]) : 2 * a + 3 ≡ 2 * b + 3 [ZMOD 5] := by
-  sorry
+  obtain ⟨x, hx⟩ := h
+  use x * 2
+  calc
+    (2 * a + 3) - (2 * b + 3) = (a - b) * 2 := by ring
+    _ = 5 * x * 2 := by rw[hx]
+    _ = 5 * (x * 2) := by ring
 
 example {m n : ℤ} (h : m ≡ n [ZMOD 4]) : 3 * m - 1 ≡ 3 * n - 1 [ZMOD 4] := by
-  sorry
+  obtain ⟨x, hx⟩ := h
+  use x * 3
+  calc
+    (3 * m - 1) - (3 * n - 1) = (m - n) * 3 := by ring
+    _ = 4 * x * 3 := by rw[hx]
+    _ = 4 * (x * 3) := by ring
 
 example {k : ℤ} (hb : k ≡ 3 [ZMOD 5]) :
     4 * k + k ^ 3 + 3 ≡ 4 * 3 + 3 ^ 3 + 3 [ZMOD 5] := by
-  sorry
+  obtain ⟨x, hx⟩ := hb
+  use x * (4 + k ^ 2 + 3 * k + 3 * 3)
+  calc
+    (4 * k + k ^ 3 + 3) - (4 * 3 + 3 ^ 3 + 3)
+      = (k - 3) * (4 + k ^ 2 + 3 * k + 3 * 3) := by ring
+    _ = 5 * x * (4 + k ^ 2 + 3 * k + 3 * 3) := by rw[hx]
+    _ = 5 * (x * (4 + k ^ 2 + 3 * k + 3 * 3)) := by ring
